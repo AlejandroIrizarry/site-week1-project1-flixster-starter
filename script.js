12833a2fea9903dd2aaa9c6b76369c286431176d
AlejandroIrizarry <alejandroirizarry@mail.com>
2023-06-08 01:41:16 -0700

    Added apiKey, pageSize var and search/movie refs.

---
+++ script.js
@@ -0,0 +1,9 @@
+//const apiKey = "MY_API_KEY"
+const apiKey = "1dd96d2fbaa44433b4338bc9d1a3ea9b"
+const pageSize = 20
+
+// Page Elements
+const searchForm = document.querySelector("search-form")
+const seachInput = document.querySelector("search-input")
+const moviesGrid = document.querySelector("movies-grid")
+
